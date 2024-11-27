cde885f21bd6a50e66cab46d0838377612ee1c53
Jane <apiwatsupaphol@gmail.com>
2024-11-27 11:15:05 +0700

    feat: init project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,14 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        yellow: {
+          100: "hsl(31, 66%, 93%)",
+          500: "hsl(39, 100%, 71%)",
+        },
+        purple: {
+          100: "hsl(254, 88%, 90%)",
+          500: "hsl(256, 67%, 59%)",
+        },
       },
     },
   },
